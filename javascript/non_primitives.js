3fb2d155eb2b944e2893be69a4cb2cdd0fc3b374
Tanvir <tanvirahmed123000@gmail.com>
2025-07-31 13:37:23 +0600

    non primitives

---
+++ javascript/non_primitives.js
@@ -0,0 +1,21 @@
+let username={
+    firstname: "Tanvir",
+    lastname: "Ahmed",
+    isLoggedIn: true,
+};
+
+console.log(username.firstname);
+console.log(username.lastname);
+console.log(typeof username); //object
+
+let today= new Date();
+console.log(today); //current date and time
+console.log(typeof today); //object
+
+
+//array
+
+let heros=["thor", "ironman", "spiderman",true];
+console.log(heros[0]); //thor
+console.log(heros[1]); //ironman 
+console.log(heros[3]); //true
