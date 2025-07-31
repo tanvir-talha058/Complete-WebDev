22da3a65855d836aaf5459a78006b325a117db8e
Tanvir <tanvirahmed123000@gmail.com>
2025-07-31 11:09:30 +0600

    primitive datatype

---
+++ javascript/logical.js
@@ -0,0 +1,13 @@
+//logical and operator
+let isloggedin= true;
+let ispaid= false;
+
+console.log(isloggedin && ispaid);
+
+
+//logical OR operator
+
+let isEmailUser= true;
+let isGoogleUser= false;
+console.log(isEmailUser || isGoogleUser);
+
---
+++ javascript/primitives.js
@@ -0,0 +1,26 @@
+//number
+
+let balance=100;
+let anotherBalance= new Number(200);
+
+console.log(typeof balance);
+console.log(anotherBalance);
+console.log(typeof anotherBalance);
+
+
+//boolean
+let isActive=true;
+let isReallyActive= new Boolean(true); //not recommended
+
+//string
+let myString="hello";
+let myString2='tanvir';
+let addStrings=myString + myString2;
+console.log(addStrings);
+console.log(myString+" " + myString2);
+console.log(typeof myString);
+console.log(typeof myString2);
+console.log(typeof addStrings);
+
+let demoOne= "value is ${ 2 * 5 }";
+console.log(demoOne); //this will not work as expected, use template literals instead
