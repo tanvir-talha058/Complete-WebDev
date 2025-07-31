6f910b5109257108e54f3e698d97debb3958b41b
Tanvir <tanvirahmed123000@gmail.com>
2025-07-31 10:42:09 +0600

    basic operations

---
+++ javascript/operations.js
@@ -0,0 +1,29 @@
+let addintion= 4+5;
+
+let score=102;
+let bonus=25;
+let totalscore= score+ bonus;
+
+let addition= 4+5;
+let substraction= 10-3;
+let multiplication= 6*7;
+let division= 20/4;
+let modulus= 10%3;
+let exponentiation= 9**34;
+
+let myscore= 100;
+myscore++;
+
+let credits=56;
+credits--;
+
+//comparison operators
+
+let num1=3;
+let num2=3;
+let num3= 6;
+
+
+console.log(num1==num2);
+console.log(num1!=num3);
+console.log(totalscore);
