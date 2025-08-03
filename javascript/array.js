37c355a526937ddb3e1cde5d5de89917692a7312
Tanvir <tanvirahmed123000@gmail.com>
2025-08-03 11:41:59 +0600

    array

---
+++ javascript/array.js
@@ -0,0 +1,26 @@
+let teaFlaviors=["green tea","black tea","olong tea"]
+
+let firsttea = teaFlaviors[0];
+console.log(firsttea);
+
+let cityes=["london","tokyo","paris","new york"]
+
+let favouriteCity=cityes[2];
+console.log(favouriteCity);
+
+let cityvisted=["dhaka","mumbai","london"]
+
+//push new elements in a array
+cityvisted.push=("delhi","satkhira");
+
+console.log(cityvisted);
+
+//pop method or delete last elements
+let teaOrder=['chai',"iced tea","green tea"];
+teaOrder.pop();
+console.log(teaOrder);
+
+let popularTea=["green tea","black tea","cold tea"];
+let softTea= popularTea;
+
+console.log(softTea);
