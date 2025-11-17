be5bf36be9270eb1110c7fed7d497dd617505be2
Tanvir <tanvirahmed123000@gmail.com>
2025-11-17 21:27:54 +0600

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./bootstrap/**/*.html",
+    "./coming soon website/**/*.html",
+    "./CSS/**/*.html",
+    "./login page/**/*.html",
+    "./Tailwind/**/*.html",
+    "./javascript/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
